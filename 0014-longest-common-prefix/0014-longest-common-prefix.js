a619ddfaab9345826f17caba8cc4ba3905183550
Ashwini Vikas Wakchaure <ashwiniwakchaure808@gmail.com>
2026-07-10 19:11:15 +0530

    Time: 5 ms (18.66%), Space: 54.7 MB (37.15%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+    let prefix = strs[0];
+
+    for (let i = 1; i < strs.length; i++) {
+        let j = 0;
+
+        while (j < prefix.length
+            && j < strs[i].length
+            && prefix[j] === strs[i][j]) {
+            j++;
+        }
+        prefix = prefix.slice(0, j);
+    }
+    return prefix;
+};
